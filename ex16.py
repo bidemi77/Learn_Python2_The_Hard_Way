77bd3afb3490ce6b882bc57e1ad6f6a3f87f9f28
Nurudeen Sulaimon <san2datamastery@gmail.com>
2018-06-16 16:03:28 -0400

    Create ex16.py

---
+++ ex16.py
@@ -0,0 +1,42 @@
+# Reading and Writing Files.
+
+from sys import argv 
+
+script,filename = argv 
+
+print "We're going to erase %r" % filename
+print "If you don't want that, hit CTRL-C (^C)."
+
+raw_input("?")
+
+print "Opening the file..."
+target = open(filename, 'w')
+
+print "Truncating the file. Goodbye!"
+target.truncate()
+
+print "Now I'm going to ask you for three lines."
+
+line1 = raw_input("line 1:")
+line2 = raw_input("line 2:")
+line3 = raw_input("line 3:")
+
+print "I'm going to write these to the file."
+
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+
+print "Any finally, we close it."
+target.close()
+
+
+# close - Closes the file. Like File -> Save..in your editor.
+# read - Reads just one line of a text file.
+# truncate - Empties the file. Watch out if you care about the file.
+# write(stuff) - writes stuff to the file.
