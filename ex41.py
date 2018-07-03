cea1d51ee839f8b4aff3673049136c702851317d
Nurudeen Sulaimon <san2datamastery@gmail.com>
2018-07-03 14:38:06 -0400

    Create ex41.py

---
+++ ex41.py
@@ -0,0 +1,19 @@
+# WORD DRILLS 
+# Class - Tell Python to make a new kind of thing.
+# Object - Two meanings: the most basic kind of thing, and any instance of some thing.
+# instance - What you get when you tell Python to create a class.
+# def - How you define a function inside a class.
+# self - Inside the functions in a class, self is a variable for the instance/ object being accessed
+# inheritance - The concept  that one class can be composed of other classes as parts,much like you and your parents.
+##composition- The concept that a class can be composed of other classes as parts, much
+# like how how a car has wheels.
+
+## atrributes- A property classes have that are from composition and are 
+# usually variables.
+
+## is-a - A phrase to say that something inherits from another, as in a "salmon" is a "fish"
+
+## has a - A phrase to say that something is composed of other  things or has a  trait, as in "a salmon has-a mouth"
+
+# Take some time to make flash cards for those and memorize them. As usual this won't make 
+# too much sense until  after you're done with this exercise, but you need to know the base words first
